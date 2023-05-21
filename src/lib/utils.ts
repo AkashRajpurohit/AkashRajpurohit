293d306c6de931110d7ea78a5708abf0881ded95
AkashRajpurohit <akashrajpurohit.009@gmail.com>
2023-05-21 10:38:46 +0530

    feat: :sparkles: basic ui components added and hooked with MDX components

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,8 @@
-export const difference = <T>(arr1: T[], arr2: T[]) =>
-  arr1.filter((x) => !arr2.includes(x));
+export const difference = <T>(arr1: T[], arr2: T[]) => arr1.filter((x) => !arr2.includes(x));
 
 export const intersection = <T>(arr: T[], ...args: T[][]): T[] =>
-  arr.filter((item: T) => args.every((arr: T[]) => arr.includes(item)));
+	arr.filter((item: T) => args.every((arr: T[]) => arr.includes(item)));
 
-export const isDateBefore = (date1: Date, date2: Date) =>
-  date1.getTime() < date2.getTime();
+export const isDateBefore = (date1: Date, date2: Date) => date1.getTime() < date2.getTime();
 
-export const isDateAfter = (date1: Date, date2: Date) =>
-  !isDateBefore(date1, date2);
+export const isDateAfter = (date1: Date, date2: Date) => !isDateBefore(date1, date2);
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -5,41 +5,41 @@ import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 import { isDateAfter } from '@lib/utils';
 
 export async function get(context: APIContext) {
-  const allPosts = await getCollection('blog');
-  const postItems = allPosts.map((post) => ({
-    title: post.data.title,
-    description: post.data.summary,
-    pubDate: post.data.lastmod || post.data.date,
-    link: `/blog/${post.slug}/`,
-    categories: post.data.tags,
-    author: 'me@akashrajpurohit.com (Akash Rajpurohit)',
-  }));
+	const allPosts = await getCollection('blog');
+	const postItems = allPosts.map((post) => ({
+		title: post.data.title,
+		description: post.data.summary,
+		pubDate: post.data.lastmod || post.data.date,
+		link: `/blog/${post.slug}/`,
+		categories: post.data.tags,
+		author: 'me@akashrajpurohit.com (Akash Rajpurohit)',
+	}));
 
-  const items = [...postItems];
+	const items = [...postItems];
 
-  let lastBuildDate = items[0].pubDate;
+	let lastBuildDate = items[0].pubDate;
 
-  for (let i = 1; i < items.length; ++i) {
-    const itemDate = items[i].pubDate;
-    if (isDateAfter(itemDate, lastBuildDate)) {
-      lastBuildDate = itemDate;
-    }
-  }
+	for (let i = 1; i < items.length; ++i) {
+		const itemDate = items[i].pubDate;
+		if (isDateAfter(itemDate, lastBuildDate)) {
+			lastBuildDate = itemDate;
+		}
+	}
 
-  const customData = `
+	const customData = `
 		<language>en-us</language>
 		<managingEditor>me@akashrajpurohit.com (Akash Rajpurohit)</managingEditor>
 		<webMaster>me@akashrajpurohit.com (Akash Rajpurohit)</webMaster>
 		<lastBuildDate>${lastBuildDate.toUTCString()}</lastBuildDate>
 	`;
 
-  return rss({
-    title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
-    drafts: false,
-    trailingSlash: true,
-    site: context.site?.toString() as string,
-    customData,
-    items,
-  });
+	return rss({
+		title: SITE_TITLE,
+		description: SITE_DESCRIPTION,
+		drafts: false,
+		trailingSlash: true,
+		site: context.site?.toString() as string,
+		customData,
+		items,
+	});
 }
