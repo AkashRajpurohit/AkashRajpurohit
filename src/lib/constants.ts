ada57fba393265ee9a610c2eebcd219228b61b87
AkashRajpurohit <akashrajpurohit.009@gmail.com>
2023-06-03 11:13:27 +0530

    pref: :zap: use webp format images for photos

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -29,11 +29,26 @@ export const allowedBlogTags = [
 export const recentPostsLimit = 4;
 
 export const heroImages = [
-  '/static/images/hero-image-1.jpg',
-  '/static/images/hero-image-2.jpg',
-  '/static/images/hero-image-3.jpg',
-  '/static/images/hero-image-4.jpg',
-  '/static/images/hero-image-5.jpg',
+  {
+    jpg: '/static/images/hero-image-1.jpg',
+    webp: '/static/images/hero-image-1.webp',
+  },
+  {
+    jpg: '/static/images/hero-image-2.jpg',
+    webp: '/static/images/hero-image-2.webp',
+  },
+  {
+    jpg: '/static/images/hero-image-3.jpg',
+    webp: '/static/images/hero-image-3.webp',
+  },
+  {
+    jpg: '/static/images/hero-image-4.jpg',
+    webp: '/static/images/hero-image-4.webp',
+  },
+  {
+    jpg: '/static/images/hero-image-5.jpg',
+    webp: '/static/images/hero-image-5.webp',
+  },
 ];
 
 export type NewsletterFormInput = {
